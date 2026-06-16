8795662858209ac80323732b056ca5a8a00c6495
satyam-git <satya.khandavalli@gmail.com>
2026-06-16 17:33:41 +0530

    Refactor VM resize script to build payload cleanly

--- Scripts/vm-resize.ps1
+++ Scripts/vm-resize.ps1
@@ -13,28 +13,36 @@ $vm = ($vmList.entities | Where-Object { $_.spec.name -eq $data.vmname } | Selec
 $vmUuid = $vm.metadata.uuid
 $currentVm = Invoke-RestMethod -Uri "$urlBase/vms/$vmUuid" -Method Get -Headers $headers
 
-# --- 2. CONSTRUCT PAYLOAD ---
-$resources = $currentVm.spec.resources
-$resources.num_vcpus = [int]$data.CPU_size
-$resources.memory_size_mib = [int]$data.mem_size * 1024
-
-if ($data.disk_action -eq "add") {
-    $maxIndex = 0
-    foreach ($d in $resources.disk_list) {
-        if ($d.device_properties.disk_address.device_index -gt $maxIndex) { $maxIndex = $d.device_properties.disk_address.device_index }
-    }
-    $resources.disk_list += @{ 
-        device_properties = @{ device_type = "DISK"; device_address = @{ adapter_type = "SCSI"; device_index = ($maxIndex + 1) } }; 
-        disk_size_bytes = [Int64]$data.size_gb * 1024 * 1024 * 1024 
+# --- 2. BUILD CLEAN PAYLOAD ---
+# We create a brand new object to send back to the API
+$spec = @{
+    name = $currentVm.spec.name
+    resources = @{
+        num_vcpus = [int]$data.CPU_size
+        memory_size_mib = [int]$data.mem_size * 1024
+        disk_list = $currentVm.spec.resources.disk_list
     }
 }
 
-# --- 3. APPLY ---
+# Add disk to the list if requested
+if ($data.disk_action -eq "add") {
+    $maxIndex = 0
+    foreach ($d in $spec.resources.disk_list) {
+        if ($d.device_properties.disk_address.device_index -gt $maxIndex) { $maxIndex = $d.device_properties.disk_address.device_index }
+    }
+    $newDisk = @{ 
+        device_properties = @{ device_type = "DISK"; disk_address = @{ adapter_type = "SCSI"; device_index = ($maxIndex + 1) } }; 
+        disk_size_bytes = [Int64]$data.size_gb * 1024 * 1024 * 1024 
+    }
+    $spec.resources.disk_list += $newDisk
+}
+
+# --- 3. APPLY CHANGES ---
 $body = @{
-    spec = @{ name = $currentVm.spec.name; resources = $resources }
+    spec = $spec
     metadata = @{ kind = "vm"; uuid = $vmUuid; project_reference = $currentVm.metadata.project_reference }
     api_version = "3.1"
 } | ConvertTo-Json -Depth 10
 
 Invoke-RestMethod -Uri "$urlBase/vms/$vmUuid" -Method Put -Headers $headers -Body $body
-Write-Host "Success: VM Updated." -ForegroundColor Green
+Write-Host "Success: VM '$($data.vmname)' updated on cluster $($data.pE_IP)." -ForegroundColor Green
